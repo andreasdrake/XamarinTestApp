4cc721867a02957c38dc3f86a40df406e421a061
Andreas Drake <andreas.drake@gmail.com>
2017-03-23 22:17:14 +0100

    Created Solution

---
+++ XamarinTestApp/XamarinTestApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿
+using XamarinTestApp.ViewModels;
+
+using Xamarin.Forms;
+
+namespace XamarinTestApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        ItemDetailViewModel viewModel;
+
+        // Note - The Xamarin.Forms Previewer requires a default, parameterless constructor to render a page.
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+        }
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+        {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = viewModel;
+        }
+    }
+}
